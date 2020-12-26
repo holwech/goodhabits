6b868a8f153bf426bd5e0335c3887c9a0303f2ec
Joachim Holwech <joachim.holwech@gmail.com>
2020-12-26 13:16:28 +0100

    Add chick response text

--- src/models/Resource.ts
+++ src/models/Resource.ts
@@ -11,4 +11,5 @@ export const chickResponses = [
   "yo you little shit it's time to move your fat bitch ass legs",
   "your mother told me that your lazy office rat body couldn't satisfy her they way i do",
   "is your office chair the pyramid and you are the pharaoh or something?",
+  "listen you little shit, do as i tell you and get out of that chair or i will crack your skull with my shell"
 ]
