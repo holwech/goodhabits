43cae02b3851d38b1734de2b11083f49d06bc3e0
Joachim Blaafjell Holwech <holwech@users.noreply.github.com>
2022-01-14 22:40:47 +0100

    Minor changes

--- src/models/Resource.ts
+++ src/models/Resource.ts
@@ -8,8 +8,5 @@ export default interface Resource {
 };
 
 export const chickResponses = [
-  'yo you little shit it\'s time to move your fat bitch ass legs',
-  'your mother told me that your lazy office rat body couldn\'t satisfy her they way i do',
-  'is your office chair the pyramid and you are the pharaoh or something?',
-  'listen you little shit, do as i tell you and get out of that chair or i will crack your skull with my shell'
+  'be humble'
 ];
